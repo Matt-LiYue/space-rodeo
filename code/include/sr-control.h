1030c2ea324922071d0ae9cc4d504de9b6e7a443
Michael Trotta <michaelstrotta@gmail.com>
2014-10-24 18:31:12 -0400

    added API to access specific models

--- code/include/sr-control.h
+++ code/include/sr-control.h
@@ -10,7 +10,14 @@ class Control{
     void setmodels(std::vector<CircleModel*>&);
     void handleEvent(sf::Event);
     bool getlevelfinished();
-  private:
-    std::vector<CircleModel*> _cirmodels;
+		
+  private:		
+		/* data members */
     bool _levelfinished;
+    std::vector<CircleModel*> _cirmodels;
+		Ship* _ship;
+		std::vector<Planet*> _planets;
+		std::vector<Cow*> _cows;
+		SpaceRanch* _ranch;
+		
 };
--- code/src/sr-control.cpp
+++ code/src/sr-control.cpp
@@ -12,134 +12,109 @@ Control::Control(){
 
 void Control::setmodels(std::vector<CircleModel*>& mymodels){
   _cirmodels = mymodels;
+	
+	/* Store circle models as their derived classes */
+  for (int i = 0; i < _cirmodels.size(); i++) {
+	  if (dynamic_cast<Ship*>(_cirmodels[i]) != 0)
+			_ship = (Ship*) _cirmodels[i];
+	  else if (dynamic_cast<SpaceRanch*>(_cirmodels[i]) != 0)
+			_ranch = (SpaceRanch*) _cirmodels[i];
+	  else if (dynamic_cast<Planet*>(_cirmodels[i]) != 0)
+			_planets.push_back((Planet*) _cirmodels[i]);
+	  else if (dynamic_cast<Cow*>(_cirmodels[i]) != 0)
+			_cows.push_back((Cow*) _cirmodels[i]);
+	}
 }
 
 bool Control::getlevelfinished(){
   return _levelfinished;
 }
 
-void Control::update(float timeInterval){
-	sf::Vector2f gravityPull;
+void Control::update(float timeInterval) {
 	
-  for (int i = 0; i < _cirmodels.size(); i++){
-    if (_cirmodels[i] -> _movable == true) {
-    	/* update positions */
-      if ((dynamic_cast<Ship*>(_cirmodels[i]) != 0) && (dynamic_cast<Ship*>(_cirmodels[i])->getState() == Ship::ORBIT)) {
-        Ship* ship = (Ship*) _cirmodels[i];
-        Planet* planet = ship->getOrbitPlanet();
-        float distance = sqrt(ship->getSpd().x*ship->getSpd().x + ship->getSpd().y*ship->getSpd().y) * timeInterval;
-        float theta = 360 * distance / (2 * M_PI * planet->getRadius());
-        ship->rotate(theta);
+	/* Ship */
+	/* map exit */
+	sf::Vector2f pos = _ship->getPosition();
+  if (pos.x < 0 || pos.y < 0 || pos.x > 800 || pos.y > 600) exit(0);
+	
+	/* movement */
+	if (_ship->getState() == Ship::ORBIT) {
+    Planet* planet = _ship->getOrbitPlanet();
+    float distance = sqrt(_ship->getSpd().x*_ship->getSpd().x + _ship->getSpd().y*_ship->getSpd().y) * timeInterval;
+    float theta = 360 * distance / (2 * M_PI * planet->getRadius());
+    _ship->rotate(theta);
+	}
+  else {
+    _ship->setPosition(_ship->getPosition() + _ship->getSpd() * timeInterval);
+	}
+	
+	/* collisions */
+	for (int j=0; j < _cows.size(); j++) {
+	  Cow* cow = _cows[j];
+		if (_ship->intersects(cow)) {
+			cow->draw = false; //TODO: better hit cow status
+		}
+	}
+	
+	if (_ship->intersects(_ranch)) {
+    std::cout << "space ranch reached" << std::endl;
+    _levelfinished = true;
+  }
+	
+	for (int j=0; j < _planets.size(); j++) {
+		Planet* planet = _planets[j];
+		if (_ship->getState() == Ship::ORBIT) break; // don't bother w/ collisions if snapped to orbit
+    sf::CircleShape gravity = planet->getGravityCircle();
+    if (_ship->getState() == Ship::FLY && _ship->intersects(&gravity)) {
+			
+      /* check for orbit or crash */
+      sf::Vector2f shipToPlanet = planet->getPosition() - _ship->getPosition();
+      sf::Vector2f dir = _ship->getSpd();
+      float dot = shipToPlanet.x * dir.x + shipToPlanet.y * dir.y;
+      sf::Vector2f projection = dir * (dot / (dir.x*dir.x + dir.y*dir.y));
+      sf::Vector2f secant = projection + _ship->getPosition() - planet->getPosition();
+		
+
+      if (sqrt(secant.x*secant.x + secant.y*secant.y) < planet->getRadius() + _ship->getRadius()) {
+      /* crash */
+        std::cout << "ship will crash into planet\n";
+        _ship -> setState(Ship::CRASH);
       }
       else {
-        _cirmodels[i] -> setPosition(_cirmodels[i]->getPosition() + _cirmodels[i]->getSpd() * timeInterval);
-      }	
-      /* check on map */
-      if (dynamic_cast<Ship*>(_cirmodels[i]) != 0) {
-        Ship* ship = (Ship*) _cirmodels[i];
-        sf::Vector2f pos = ship->getPosition();
-        if (pos.x < 0 || pos.y < 0 || pos.x > 800 || pos.y > 600) exit(0);
-      }		 
-      /* check intersections */
-      for (int j = 0; j < _cirmodels.size(); j++) {
-        if (i == j) continue;
-      /* real intersection */
-        if (_cirmodels[i]->intersects(_cirmodels[j]) && dynamic_cast<Ship*>(_cirmodels[i])->getState() != Ship::ORBIT) {//no hit when orbiting
-          if (dynamic_cast<Ship*>(_cirmodels[i]) != 0) {
-            if (dynamic_cast<Ship*>(_cirmodels[i])->getState() == Ship::CRASH) exit(0);
-	    //std::cout << "ship has hit something\n"; 
-            if (dynamic_cast<Cow*>(_cirmodels[j]) != 0) {
-              /* handle cow hit */
-              //std::cout << "hit cow" << std::endl;
-              _cirmodels[j]->draw = false; ; // temp erase
-            }	
-            else if (dynamic_cast<SpaceRanch*>(_cirmodels[j]) != 0) {
-              std::cout << "space ranch reached" << std::endl;
-              _levelfinished = true;
-            }
-          }
-					
-				/*
-					numLives--;
-					if (numLives == 0) gameOver();
-				  else restartLevel();
-				*/
-        }
-				
-        /* gravity intersection */
-          if (dynamic_cast<Ship*>(_cirmodels[i]) != 0 && dynamic_cast<Planet*>(_cirmodels[j]) != 0) {
-            Planet* planet = (Planet*) _cirmodels[j];
-            Ship* ship = (Ship*) _cirmodels[i];
-            sf::CircleShape gravity = planet->getGravityCircle();
-            if (ship->getState() == Ship::FLY && ship->intersects(&gravity)) {
-           /* check for orbit or crash */
-              sf::Vector2f shipToPlanet = planet->getPosition() - ship->getPosition();
-              sf::Vector2f dir = ship->getSpd();
-              float dot = shipToPlanet.x * dir.x + shipToPlanet.y * dir.y;
-              sf::Vector2f projection = dir * (dot / (dir.x*dir.x + dir.y*dir.y));
-              sf::Vector2f secant = projection + ship->getPosition() - planet->getPosition();
-						
-						/* dbg
-						std::cout << "Planet: " << planet->getPosition().x << "," << planet->getPosition().y << std::endl;
-						std::cout << "Ship: " << ship->getPosition().x << "," << ship->getPosition().y << std::endl;
-						std::cout << "Dir: " << dir.x << "," << dir.y << std::endl;
-						std::cout << "Ship2Planet: " << shipToPlanet.x << "," << shipToPlanet.y << std::endl;
-						std::cout << "dot: " << dot << std::endl;
-						std::cout << "projection: " << projection.x << "," << projection.y << std::endl;
-						std::cout << "secant: " << secant.x << "," << secant.y << std::endl;
-						*/
-              if (sqrt(secant.x*secant.x + secant.y*secant.y) < planet->getRadius() + ship->getRadius()) {
-              /* crash */
-                std::cout << "ship will crash into planet\n";
-                ship -> setState(Ship::CRASH);
-              }
-              else {
-              /* orbit */
-                std::cout << "ship in orbit. state: " << ship -> getState() << "\n";
-                ship -> setState(Ship::ORBIT);
-                ship -> setOrbit(planet);
-              }
-            }
-            else if (ship->getState() == Ship::ORBIT) { 
-            /* Don't perform any gravity intersection calculations */
-            }
-          } 
-        }
-    } 
-  }
-}
-
-
-void Control::handleEvent(sf::Event event){
-  Ship * ship;
-  for (int i = 0 ; i < _cirmodels.size(); i++ ){
-    if (dynamic_cast<Ship*>(_cirmodels[i]) != 0){ 
-      ship = (Ship *) _cirmodels[i];
+      /* orbit */
+        std::cout << "ship in orbit. state: " << _ship -> getState() << "\n";
+        _ship -> setState(Ship::ORBIT);
+        _ship -> setOrbit(planet);
+      }
     }
-  }
+	}
+}
+	
+void Control::handleEvent(sf::Event event){
+
   if (event.key.code == sf::Keyboard::Space){ // Fire the rocket
-      if (ship -> getState() == Ship::REST){//fire
-        ship -> adjustSpd(100);
-        ship -> setState(Ship::FLY);
+      if (_ship -> getState() == Ship::REST){//fire
+        _ship -> adjustSpd(100);
+        _ship -> setState(Ship::FLY);
       }
-      else if (ship -> getState() == Ship::FLY){//burst, TODO: Need a counter
-        ship -> adjustSpd(300);
-        ship -> setState(Ship::BURST);
+      else if (_ship -> getState() == Ship::FLY){//burst, TODO: Need a counter
+        _ship -> adjustSpd(300);
+        _ship -> setState(Ship::BURST);
       }
-      else if (ship -> getState() == Ship::ORBIT) {
-        ship -> adjustSpd(300);
-        ship -> quitOrbit();
-	ship -> setState(Ship::BURST);
+      else if (_ship -> getState() == Ship::ORBIT) {
+        _ship -> adjustSpd(300);
+        _ship -> quitOrbit();
+	      _ship -> setState(Ship::BURST);
       }
   }
   if (event.key.code == sf::Keyboard::Left){
-    if (ship -> getState() == Ship::REST){
-      ship -> rotate(-3); 
+    if (_ship -> getState() == Ship::REST){
+      _ship -> rotate(-3); 
     }
   }
   if (event.key.code == sf::Keyboard::Right){
-    if (ship -> getState() == Ship::REST){
-      ship -> rotate(3);
+    if (_ship -> getState() == Ship::REST){
+      _ship -> rotate(3);
     }
   } 
 }
